e9980c3ff5d9715f1e97a5332373d6508838d5bf
abharel <44101009+abharel@users.noreply.github.com>
2018-11-11 13:50:16 -0600

    new file

---
+++ erc721.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.19;
+
+contract StemCapstone {
+    uint256 Seed=int16;
+    uint256 Plant=int16***Seed;
+    
+    struct Crops{
+        string plants
+        uint seeds
+    }
+    
+    Wheat[] public crops;
+    fuction grow(string _name,uint _Seed){
+    
+    Corn[] public crops;
+    fuction grow(string _name,uint _Seed){
+    
+    function _generateRandomSeeds (string _str) private view returns (uint) {
+    }
+    
